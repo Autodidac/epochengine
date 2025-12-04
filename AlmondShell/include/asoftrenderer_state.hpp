3742a62a5ca739e4a5b400706778b04d67b9c972
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2025-12-04 10:35:48 -0500

    Fix module imports and timer qualifiers

--- AlmondShell/include/asoftrenderer_state.hpp
+++ AlmondShell/include/asoftrenderer_state.hpp
@@ -74,8 +74,8 @@ namespace almondnamespace::anativecontext
         } keyboard;
 
         // Timing
-        time::Timer pollTimer = time::createTimer(1.0);
-        almondnamespace::time::Timer fpsTimer = time::createTimer(1.0);
+        almondnamespace::time::Timer pollTimer = almondnamespace::time::createTimer(1.0);
+        almondnamespace::time::Timer fpsTimer = almondnamespace::time::createTimer(1.0);
         int frameCount = 0;
 
         // Cube rotation
